6651c5d661d7cfb0700a2026e1595e7bb96f4cae
Chandrakanthreddy77 <chandrakanthreddy.y@publicissapient.com>
2020-03-24 19:21:38 +0530

    Cmmit changes

--- ResolveConflicts/src/main/java/com/chandra/learnings/Sample.java
+++ ResolveConflicts/src/main/java/com/chandra/learnings/Sample.java
@@ -3,7 +3,8 @@ package com.chandra.learnings;
 public class Sample {
 
 	public static void main(String[] args) {
-		System.out.println("Chandra");
+		System.out.println("chandra12");
+		System.out.println("master12");
 	}
 	
 }
