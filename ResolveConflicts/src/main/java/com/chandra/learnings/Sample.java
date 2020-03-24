d2e3113ba9c9d00521127afe23293d1fd2dc5129
Chandrakanthreddy77 <chandrakanthreddy.y@publicissapient.com>
2020-03-24 19:34:22 +0530

    g

--- ResolveConflicts/src/main/java/com/chandra/learnings/Sample.java
+++ ResolveConflicts/src/main/java/com/chandra/learnings/Sample.java
@@ -3,7 +3,7 @@ package com.chandra.learnings;
 public class Sample {
 
 	public static void main(String[] args) {
-		System.out.println("master12");
+		System.out.println("mastermaster master");
 		System.out.println("chandra12");
 		System.out.println("chandra1");
 		System.out.println("master");
